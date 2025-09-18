22a3c0b29218eb75978f7f6da72e5f12f044d42c
Злата Бортникова <bortnikova.zlata@mail.ru>
2025-09-18 17:28:40 +0300

    Add files via upload

--- Lab1/Blue.cs
+++ Lab1/Blue.cs
@@ -1,4 +1,6 @@
-﻿namespace Lab1
+﻿using System.Numerics;
+
+namespace Lab1
 {
     public class Blue
     {
@@ -7,7 +9,13 @@
             bool answer = false;
 
             // code here
-
+            if (a != 0 || b!=0)
+            {
+                if ((a>0 && b>0) || (a < 0 && b < 0))
+                {
+                    answer = true;
+                }
+            }
             // end
 
             return answer;
@@ -17,7 +25,11 @@
             bool answer = false;
 
             // code here
-
+            int compareD = (int)d;
+            if (compareD != d)
+            {
+                answer = true;
+            }
             // end
 
             return answer;
@@ -25,9 +37,12 @@
         public bool Task3(int a, int b)
         {
             bool answer = false;
-
+            
             // code here
-
+            if (b != 0 && a%b==0)
+            {
+                answer = true;
+            }
             // end
 
             return answer;
@@ -37,30 +52,73 @@
             double answer = 0;
 
             // code here
+            double absD = Math.Abs(d);
+            double absF = Math.Abs(f);
+            double absG = Math.Abs(g);
+            if (absD >= absF && absD >= absG)
+            {
+                answer = d;
+            }
+            else
+            {
+                if (absF >= absD && absF >= absG)
+                {
+                    answer = f;
+                }
+                else
+                {
+                    if (absG >= absD && absG >= absF)
+                    {
+                        answer = g;
+                    }
+                }
 
-            // end
+            }
+                // end
 
-            return answer;
+                return answer;
         }
         public double Task5(double x)
         {
             double answer = 0;
 
             // code here
+            if (x > 0)
+            {
+                answer = 1;
+            }
+            else
+            {
+                if (x > -1 && x <= 0)
+                {
+                    answer = x + 1;
+                }
+                else
+                {
+                    if (x <= -1)
+                    {
+                        answer = 0;
+                    }
+                }
+            }
+                // end
 
-            // end
-
-            return answer;
+                return answer;
         }
         public bool Task6(double circleS, double squareS)
         {
             bool answer = false;
 
             // code here
+            double circleD= Math.Sqrt(circleS/Math.PI) * 2;
+            double squareD = Math.Sqrt(squareS);
+            if (circleD <= squareD)
+            {
+                answer = true;
+            }
+            //end
 
-            // end
-
-            return answer;
+                return answer;
         }
 
         public double Task7(double d, double f)
@@ -68,20 +126,50 @@
             int answer = 0;
 
             // code here
+            if (Math.Abs(d) < Math.Abs(f))
+            {
+                if (d > 0)
+                {
+                    answer = -1;
+                }
+            }
+            else
+            {
+                if (f > 0)
+                {
+                    answer = 1;
+                }
+            }
+                // end
 
-            // end
-
-            return answer;
+                return answer;
         }
         public bool Task8(int a, int b, int c)
         {
             bool answer = false;
 
             // code here
-            
-            // end
 
-            return answer;
+            int old = a / 2;
+            int middle = b / 2;
+            int young = c / 2;
+            int total = old + middle + young;
+            if (total % 3 == 0 && total !=0)
+            {
+                answer = true;
+            }
+            else
+            {
+                if ((total + 1) % 3 == 0 && total!=0)
+                {
+                    answer = true;
+                }
+            }
+
+
+                // end
+
+                return answer;
         }
     }
 }
